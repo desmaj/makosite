f2d92a1f3532f2973ebc4893067b4c232911580f
Matthew Desmarais <matthew.desmarais@gmail.com>
2020-02-04 13:53:52 -0500

    Added details to the setup.py.

--- setup.py
+++ setup.py
@@ -1,28 +1,36 @@
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import find_packages
+from setuptools import setup
 
-version = '0.0'
 
-setup(name='makosite',
-      version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      [console_scripts]
-      mksite = makosite.run:main
-      mksite-import = makosite.importer:main
-      """,
-      )
+VERSION = '0.0'
+DESCRIPTION = """
+Simple website builder using mako templates.
+
+
+"""
+
+setup(
+    name='makosite',
+    version=VERSION,
+    description=DESCRIPTION.strip().splitlines()[0],
+    long_description=DESCRIPTION.strip(),
+    classifiers=[],
+    keywords='',
+    author='Matthew Desmarais',
+    author_email='matthew.desmarais@gmail.com',
+    url='https://github.com/desmaj/makosite',
+    license='GPLv3',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'beautifulsoup4>=4.8.2',
+        'Click>=7.0',
+        'Mako>=1.1.0',
+    ],
+    entry_points="""
+    [console_scripts]
+    mksite = makosite.run:main
+    mksite-import = makosite.importer:main
+    """,
+)
